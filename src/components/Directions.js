a08610addf196b845bd7c3f437237146b0baf4f8
cramerj1470 <john@Cramerec.com>
2025-01-23 00:50:38 -0500

    fixing buttons

--- src/components/Directions.js
+++ src/components/Directions.js
@@ -6,7 +6,7 @@ function Directions() {
       <ul>Directions
         <li>If You want to add you own words press the Make My Own Words button and enter your (10) words</li>
         <li>Press Start button to begin</li>
-        <li>click letters of word starring with the first word</li>
+        <li>click letters of word starting with the first word</li>
         <li>Click Found Word button when you have selected all the letters of the word you found</li>
         <li>Have fun!</li>
         <li>We will be adding more functionality/features</li>
