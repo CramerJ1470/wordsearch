186e91ba111122a75fd8d8c46ff80a8e47d21505
cramerj1470 <john@Cramerec.com>
2025-01-27 00:16:27 -0500

    removed consolelogs

--- src/components/Directions.js
+++ src/components/Directions.js
@@ -8,7 +8,6 @@ function Directions({grid,wordList}) {
   let letterIndex = 0;
 
   function startPlay() {
-    console.log("start Play wordList:",wordList);
     document.getElementById("startplay").classList.add("hiddenorig");
     document.getElementById("foundword").classList.remove("hiddenorig");
     document.getElementById("restartgame").classList.remove("hiddenorig");
@@ -27,23 +26,15 @@ function Directions({grid,wordList}) {
        function changecolor(e) {
          let letterToCheck = e.target.textContent;
          let letterBlock = Number(e.target.name);
-         console.log("Letterblock:",letterBlock);
          e.target.classList.add("white");    
-         console.log(letterToCheck);   
-         console.log("checkedList:",checkedList) ;
-         console.log("letterIndexCC: ",letterIndex);
          for (let wc = 0; wc < checkedList.length; wc++) {
-             console.log("wc :",wc);
-
- 
-             console.log(checkedList[wc][letterIndex]);
-             if (checkedList[wc][letterIndex] === letterToCheck) {
+          if (checkedList[wc][letterIndex] === letterToCheck) {
                  if (pickedWord.length == 0  && letterIndex==0) {pickedWord = letterToCheck;} 
                  else if (pickedWord.length == letterIndex) {pickedWord=pickedWord+letterToCheck; }  
                  newWordList.push(checkedList[wc]); 
-                 console.log("inside newWorldList:",newWordList);
+           
               
-             } else if (checkedList.length === 0 || wc == checkedList.length-1 && pickedWord.length == letterIndex) {letterIndex = 0;console.log("letterIndex:",letterIndex);  console.log("start again!");letterBlock.classList.remove("white"); console.log("pickedWord elseIf:",pickedWord); pickedWord= "";console.log("checkedList elseif: ",checkedList);checkedList=[];wc=checkedList.length;
+             } else if (checkedList.length === 0 || wc == checkedList.length-1 && pickedWord.length == letterIndex) {letterIndex = 0;letterBlock.classList.remove("white");  pickedWord= "";checkedList=[];wc=checkedList.length;
              } 
              // else if(){}
          }
@@ -56,16 +47,15 @@ function Directions({grid,wordList}) {
       
       function foundWord() {
          
-        if (wordList.length ===1 && wordList[0] === pickedWord) { document.getElementById(`${pickedWord}`).style.color="rgb(95, 38, 109)";alert("You've Found All The Words!!!"); document.getElementById("makeownwords").classList.remove("hiddenorig");} else if (wordList.includes(pickedWord)) { alert(`You found ${pickedWord}`); document.getElementById(`${pickedWord}`).style.color="rgb(95, 38, 109)";console.log("word");
+        if (wordList.length ===1 && wordList[0] === pickedWord) { document.getElementById(`${pickedWord}`).style.color="rgb(95, 38, 109)";alert("You've Found All The Words!!!"); document.getElementById("makeownwords").classList.remove("hiddenorig");} else if (wordList.includes(pickedWord)) { alert(`You found ${pickedWord}`); document.getElementById(`${pickedWord}`).style.color="rgb(95, 38, 109)";
         wordList = wordList.filter(e => e !== checkedList[0]);
-        console.log("shorter checkedList:",wordList);} else {alert("Word is not on the list");console.log("checkedList:",checkedList,",wordlist:",wordList,",letterIndex:",letterIndex);}
+        } else {alert("Word is not on the list");}
         
        
        
         pickedWord ='';
         newWordList = [];
         checkedList = wordList;
-        console.log("new CheckList:",checkedList); 
         letterIndex=0;
        
      }
